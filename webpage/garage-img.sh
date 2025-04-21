b209d0feed233711787b928a0f37e29c704a50f0
smp46 <me@smp46.me>
2025-04-21 10:54:58 +1000

    Added webpage and save for training bash script already in use

---
+++ webpage/garage-img.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Read the image URL from an environment variable
+IMAGE_URL="http://admin:@192.168.69.66/Snapshot/1/1/RemoteImageCaptureV2?ImageFormat=jpg"
+
+# Output directory
+OUTPUT_DIR="/home/smp/garage"
+OUTPUT_FILE="${OUTPUT_DIR}/garage.jpg"
+
+# Create output directory if it doesn't exist
+mkdir -p "${OUTPUT_DIR}"
+
+# Download the image using curl
+curl -o "${OUTPUT_FILE}" "${IMAGE_URL}"
+
